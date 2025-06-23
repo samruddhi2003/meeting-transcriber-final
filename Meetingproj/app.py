b33f47263ab9a577dc054071f39ab5477fd6dda0
Samruddhi kukarni <96710416+samruddhi2003@users.noreply.github.com>
2025-06-23 23:26:23 +0530

    Update app.py

--- Meetingproj/app.py
+++ Meetingproj/app.py
@@ -28,11 +28,12 @@ if uploaded_file is not None:
 
     st.subheader("📄 Summary")
     st.write(summary)
+
     try:
     output = subprocess.check_output(["ffmpeg", "-version"]).decode()
     st.text("✅ FFmpeg is installed!")
     st.text(output.splitlines()[0])
-except Exception as e:
+    except Exception as e:
     st.error(f"❌ FFmpeg check failed: {e}")
 
     
